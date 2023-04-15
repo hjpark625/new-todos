13f36281049894e5d6a79862e3e026e181006b66
hjpark625 <zero950@naver.com>
2023-04-15 14:47:46 +0900

    feat: baseURL 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://new-todos.site/api' : 'http://localhost:4000/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost/api',
+  baseURL,
   withCredentials: true,
 });
 
