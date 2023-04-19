247c6a7ba45a45e8f81d3059395d30fa485341cf
jeff <jeff@moduparking.com>
2023-04-19 09:40:32 +0900

    refactor: baseURL 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' ? 'https://new-todos.site/api' : 'http://localhost:4000/api';
+const baseURL = 'http://localhost:4000/api';
 
 const api = axios.create({
   baseURL,
