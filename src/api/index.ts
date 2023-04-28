2490f6b6d6b1de39a2c135441c2e379df5662240
jeff <jeff@moduparking.com>
2023-04-28 16:15:22 +0900

    feat: 토큰인증 방식 변경

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,9 +2,14 @@ import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost/api' : 'http://0.0.0.0/api';
 
+const token = localStorage.getItem('access_token');
+
 const api = axios.create({
   baseURL,
   withCredentials: true,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
 
 export default api;
